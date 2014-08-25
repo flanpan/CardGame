ffb5abb6652ea435e9dc72a17592e54d6be9d251
unknown <feng.pan@ZY-PANFENG.cgi.com>
2014-08-25 15:06:52 +0800

    Rejected commit: Default

--- game-server/app.js
+++ game-server/app.js
@@ -1,2 +1,51 @@
-require('coffee-script/register');
-require('./main');
+var pomelo = require('pomelo');
+var bearcat = require('bearcat');
+/**
+ * Init app for client.
+ */
+var app = pomelo.createApp();
+
+var Configure = function() {
+    app.set('name', 'CardGame');
+
+    // app configuration
+    app.configure('production|development', 'connector', function() {
+        app.set('connectorConfig', {
+            connector: pomelo.connectors.hybridconnector,
+            heartbeat: 3,
+            useDict: true//,
+            // useProtobuf: true
+        });
+    });
+
+    app.configure('production|development', 'gate', function() {
+        app.set('connectorConfig', {
+            connector: pomelo.connectors.hybridconnector//,
+            // useProtobuf: true
+        });
+    });
+
+    // app configure
+    app.configure('production|development', function() {
+        // route configures
+        var routeUtil = bearcat.getBean('routeUtil');
+
+        app.route('chat', routeUtil.chat.bind(routeUtil));
+
+        // filter configures
+        app.filter(pomelo.timeout());
+    });
+};
+
+var contextPath = require.resolve('./context.json');
+bearcat.createApp([contextPath]);
+
+bearcat.start(function() {
+    Configure();
+    // start app
+    app.start();
+});
+
+process.on('uncaughtException', function(err) {
+    console.error(' Caught exception: ' + err.stack);
+});
