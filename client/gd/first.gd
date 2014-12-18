60ec3cabdc25752f65a840c72adb69107863b6ab
flanpan <312631376@qq.com>
2014-12-19 00:25:43 +0800

    fix bug

--- client/gd/first.gd
+++ client/gd/first.gd
@@ -4,17 +4,19 @@ var isNeedCopy = false
 
 # bug Directory.dir_exists
 # bug Directory.copy at android
+# bug Globals.set_resource_path can not use at _init()
 
 var global
 func _init():
+	pass
+	
+func _ready():
 	var file = File.new()
 	if isPlatformNeedCopy():
 		if not file.file_exists('user://'+Globals.get('user/fileInfoName')):
 			isNeedCopy = true
 		else:
 			Globals.set_resource_path(OS.get_data_dir())
-	
-func _ready():
 	pass
 
 func isPlatformNeedCopy():
